a5d14753a052644657ce784c9bbdf2ad2357ac1e
Thibault Cohen <titilambert@ttb.lt>
2022-02-06 14:57:52 -0500

    Adding more sensors

--- hydroqc2mqtt/contract_device.py
+++ hydroqc2mqtt/contract_device.py
@@ -5,133 +5,8 @@ from mqtt_hass_base.device import MqttDevice
 from hydroqc.webuser import WebUser
 
 from hydroqc2mqtt.__version__ import VERSION
+from hydroqc2mqtt.sensors import SENSORS, BINARY_SENSORS
 
-
-SENSORS = {
-    "balance": {
-        "name": "Balance",
-        "data_source": "account.balance",
-        "device_class": "monetary",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:currency-usd",
-        "state_class": "measurement",
-        "unit": "$",
-        "sub_mqtt_topic": "acccount/state",
-    },
-    "wc_cumulated_credit": {
-        "name": "Cumulated Winter Credit",
-        "data_source": "contract.winter_credit.value_cumulated_credit",
-        "device_class": "monetary",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:currency-usd",
-        "state_class": "measurement",
-        "unit": "$",
-        "sub_mqtt_topic": "wintercredits/state",
-    },
-    "wc_next_anchor_start": {
-        "name": "Next Anchor Period Start",
-        "data_source": "contract.winter_credit.value_next_periods_anchor_start_iso",
-        "device_class": "timestamp",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:clock-start",
-        "sub_mqtt_topic": "wintercredits/next/anchor",
-
-    },
-    "wc_next_anchor_end": {
-        "name": "Next Anchor Period End",
-        "data_source": "contract.winter_credit.value_next_periods_anchor_end_iso",
-        "device_class": "timestamp",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:clock-end",
-        "sub_mqtt_topic": "wintercredits/next/anchor",
-
-    },
-    "wc_next_peak_start": {
-        "name": "Next Peak Period Start",
-        "data_source": "contract.winter_credit.value_next_periods_peak_start_iso",
-        "device_class": "timestamp",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:clock-start",
-        "sub_mqtt_topic": "wintercredits/next/peak",
-
-    },
-    "wc_next_peak_end": {
-        "name": "Next Peak Period End",
-        "data_source": "contract.winter_credit.value_next_periods_peak_end_iso",
-        "device_class": "timestamp",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:clock-end",
-        "sub_mqtt_topic": "wintercredits/next/peak",
-
-    },
-    "wc_next_critical_event_start": {
-        "name": "Next Critical Event Start",
-        "data_source": "contract.winter_credit.value_next_start_iso",
-        "device_class": "timestamp",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:clock-start",
-        "sub_mqtt_topic": "wintercredits/next/event",
-    },
-    "wc_next_critical_event_end": {
-        "name": "Next Critical Event End",
-        "data_source": "contract.winter_credit.value_next_end_iso",
-        "device_class": "timestamp",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:clock-end",
-        "sub_mqtt_topic": "wintercredits/next/event",
-    }
-}
-BINARY_SENSORS = {
-    "wc_critical": {
-        "name" : "Critical",
-        "data_source": "contract.winter_credit.value_state_critical",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:flash-alert",
-        "sub_mqtt_topic": "wintercredits/state",
-    },
-    "wc_event_in_progress": {
-        "name": "Event In Progress",
-        "data_source": "contract.winter_credit.value_state_event_in_progress",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:flash-alert",
-        "sub_mqtt_topic": "wintercredits/state",
-    },
-    "wc_pre_heat": {
-        "name": "Pre-heat In Progress",
-        "data_source": "contract.winter_credit.value_state_pre_heat",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:flash-alert",
-        "sub_mqtt_topic": "wintercredits/state",
-    },
-    "wc_next_anchor_critical": {
-        "name": "Next Anchor Period Critical",
-        "data_source": "contract.winter_credit.value_next_periods_anchor_critical",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:flash-alert",
-        "sub_mqtt_topic": "wintercredits/state",
-    },
-    "wc_next_peak_critical": {
-        "name": "Next Peak Period Critical",
-        "data_source": "contract.winter_credit.value_next_periods_peak_critical",
-        "expire_after": 0,
-        "force_update": False,
-        "icon": "mdi:flash-alert",
-        "sub_mqtt_topic": "wintercredits/state",
-    }
-    
-}
 
 class HydroqcContractDevice(MqttDevice):
 
@@ -222,6 +97,7 @@ class HydroqcContractDevice(MqttDevice):
         customer = self._webuser.get_customer(self._customer_id)
         account = customer.get_account(self._account_id)
         contract = account.get_contract(self._contract_id)
+        await contract.get_periods_info()
         await contract.winter_credit.refresh_data()
         # Send sensor state one by one ...
         for sensor_key in self._config.get("sensors", []):
