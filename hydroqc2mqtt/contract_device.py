0e7a25b5aa7086aa1227d4cce9e0e2daf7165447
Thibault Cohen <titilambert@ttb.lt>
2022-02-06 19:49:40 -0500

    Improve logging

--- hydroqc2mqtt/contract_device.py
+++ hydroqc2mqtt/contract_device.py
@@ -110,7 +110,7 @@ class HydroqcContractDevice(MqttDevice):
                 if index + 1 == len(datasource[1:]):
                     value = data_obj
             if value is None:
-                raise Exception("Can not find value")
+                raise Exception(f"Can not find value for: {sensor_key}")
 
             entity = getattr(self, sensor_key)
             entity.send_state(value, {})
