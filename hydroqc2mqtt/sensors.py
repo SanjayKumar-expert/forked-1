a90781d9f3c57bc42ff872b96f165ec8e6bc4592
mdallaire <git@dlai.re>
2022-12-14 21:24:54 +0000

    Remove state class from current period

--- hydroqc2mqtt/sensors.py
+++ hydroqc2mqtt/sensors.py
@@ -259,7 +259,6 @@ SENSORS: dict[
         "expire_after": 0,
         "force_update": False,
         "icon": None,
-        "state_class": "measurement",
         "unit": None,
         "sub_mqtt_topic": "wintercredits/state",
     },
